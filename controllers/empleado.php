389b327182032dd812c3a09234ae38b5fe93d67d
JhoannS <sebastianzamudio2004@gmail.com>
2024-10-31 12:19:46 -0500

    implementacion de perfil de empleado con restrecciones de uso OCTUBRE 31 DE 2024, 12:19 pm

--- controllers/empleado.php
+++ controllers/empleado.php
@@ -1,14 +1,18 @@
 <?php
 
 class Empleado extends SessionController {
+    protected $user;
+
     
     function __construct(){
         parent::__construct();
+        $this->user = $this->getUserSessionData();
         error_log('Empleado::construct -> Inicio del controlador Empleado');
     }
 
     function render(){
         error_log('Empleado::render -> Cargando vista de indexEmpleado');
-        $this->view->render('empleado/indexEmpleado');
+        $user = $this->getUserSessionData(); // Obtén los datos del usuario
+        $this->view->render('empleado/indexEmpleado', ['user' => $user]); 
     }
 }
