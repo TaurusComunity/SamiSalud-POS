7e70dcda616ca12817f20e3cec88a6c450e2f969
JhoannS <sebastianzamudio2004@gmail.com>
2024-12-23 09:01:06 -0500

    Configuracion de modales, Dic 23 09:00

--- views/partials/modalConsolidadoFaltantes.php
+++ views/partials/modalConsolidadoFaltantes.php
@@ -52,7 +52,7 @@
 
                 <div class="flex justify-center gap-5 mt-2">
 
-                    <button onclick="document.getElementById('myModalConsolidado').close();"
+                    <p onclick="document.getElementById('myModalConsolidado').close();"
                         class="flex items-center bg-white border-2 border-rojo text-rojo gap-1 px-10 py-2  mt-4  cursor-pointer text-gray-800 font-semibold tracking-widest rounded-md hover:bg-white hover:text-rojo duration-300 hover:gap-2 hover:translate-x-3">
                         Volver
                         <svg class="w-5 h-5" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
@@ -61,7 +61,7 @@
                                 d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
                                 stroke-linejoin="round" stroke-linecap="round"></path>
                         </svg>
-                    </button>
+                    </p>
 
 
 
